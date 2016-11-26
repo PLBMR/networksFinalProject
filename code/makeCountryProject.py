b15f350a4363ddbbc168832c75c5a9aa513ba576
PLBMR <mmrosenb@andrew.cmu.edu>
2016-11-25 23:07:52 -0500

    Initialized country projection script

---
+++ code/makeCountryProject.py
@@ -0,0 +1,12 @@
+#makeCountryProjection.py
+#helper script that takes the cleaned full network and projecting it into a
+#country-based network.
+
+#imports
+
+#helpers
+
+#main process
+
+if __name__ == "__main__":
+    print "Country-Projection Built."
